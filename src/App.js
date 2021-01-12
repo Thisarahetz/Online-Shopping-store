7ab32643126fb047bfd3341e257491391bae5710
Thisarahetz <thisarayashodagcc@gmail.com>
2021-01-13 00:18:00 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Pages/Heder/Navbar';
+import Navbar from './Componet/Heder/Navbar';
+import About from './Componet/About';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
+
 
 function App() {
   return (
-   <Navbar></Navbar>
+    <div className="container">
+      
+      <BrowserRouter>
+      <Navbar></Navbar>
+      <Switch>
+              
+              <Route path="/about"><About></About></Route>
+              
+      </Switch>
+      </BrowserRouter>
+
+    </div>
+   
   );
 }
 
--- src/Componet/Heder/Navbar.jsx
+++ src/Componet/Heder/Navbar.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 
 function Navbar(){
     return(
+      
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
         <a href="" className="navbar-brand">Online Store</a>
         <button className="navbar-toggler" data-toggle="collapse" data-target="#ccsl"><span className="navbar-toggler-icon"></span></button>
         <div className="collapse navbar-collapse justify-content-center" id="ccsl">
           <ul className="navbar-nav">
-            <li clasNaclassNames="nav-item"><a href="" className="nav-link">Home</a></li>
-            <li className="nav-item active"><a href="" class="nav-link">About</a></li>
-            <li className="nav-item"><a href="" className="nav-link">Shop</a></li>
-            <li className="nav-item"><a href="" className="nav-link">Help</a></li>
+            <li clasNaclassNames="nav-item"><Link href="" className="nav-link">Home</Link></li>
+            <li className="nav-item active"><Link to="/about" class="nav-link">About</Link></li>
+            <li className="nav-item"><Link href="" className="nav-link">Shop</Link></li>
+            <li className="nav-item"><Link href="" className="nav-link">Help</Link></li>
           </ul>
         </div>
         <button className="btn btn-sm btn-light">Your Cart</button>
